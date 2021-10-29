444b484c2b74023712db07204720f5c9706bc995
Talis Aparecido dos Reis <talisapreis@gmail.com>
2021-10-29 09:13:49 -0300

    Refactor: Refactored Code

--- CalculatorWeb/inputHandler.js
+++ CalculatorWeb/inputHandler.js
@@ -1,54 +1,71 @@
-let _storingValues = [];
+let calculator = (() => {
+  let _storingValues = [];
+  let _total = 0;
 
-const getOperations = {
-  sum: (num1, num2) => num1 + num2,
-  sub: (num1, num2) => num1 - num2,
-  mult: (num1, num2) => num1 * num2,
-  divs: (num1, num2) => num1 / num2,
-};
+  const getOperations = {
+    sum: (num1, num2) => num1 + num2,
+    sub: (num1, num2) => num1 - num2,
+    mult: (num1, num2) => num1 * num2,
+    divs: (num1, num2) => num1 / num2,
+  };
 
-_getInputValue = () => {
-  const input = document.querySelector("#entryValue");
-  return +input.value;
-};
+  const _callDom = () => {
+    const input = document.querySelector("#entryValue");
+    const radio = document.querySelector('input[type="radio"]');
+    const operacao = document.querySelector('input[type="radio"]:checked');
+    const totalSpan = document.querySelector("span");
+    const modalSucess = document.querySelector(".modal-sucess");
+    return {
+      input,
+      radio,
+      operacao,
+      totalSpan,
+      modalSucess
+    };
+  };
 
-_clearInput = () => {
-  const input = document.querySelector("#entryValue");
-  input.value = "";
-};
+  const _getInputValue = () => {
+    let input = _callDom().input;
+    return +input.value;
+  };
 
-_storage = () => {
-  if (_getInputValue() !== 0) {
-    _storingValues = [..._storingValues, _getInputValue()];
-  }
-  return _storingValues;
-};
+  const clearInput = () => {
+    let input = _callDom().input;
+    input.value = "";
+  };
 
-_resetStorage = () => (_storingValues = []);
+  const storage = () => {
+    if (_getInputValue() !== 0) {
+      _storingValues = [..._storingValues, _getInputValue()];
+    }
+    return _storingValues;
+  };
 
-_operation = () => {
-  const operacao = document.querySelector('input[type="radio"]:checked').value;
-  document.querySelector('input[type="radio"]').checked = true;
-  let total = 0;
-  total = _storage().reduce(getOperations[operacao]);
-  return total;
-};
+  const _resetStorage = () => (_storingValues = []);
 
-_openResult = () =>
-  (document.querySelector(".modal-sucess").className =
-    "modal-sucess --is-sucess");
-_closeModalSucess = () =>
-  document.querySelector(".modal-sucess").classList.remove("--is-sucess");
-_returnSucess = () =>
-  setTimeout(function () {
-    _closeModalSucess();
+  const _operation = () => {
+    _total = storage().reduce(getOperations[_callDom().operacao.value]);
+    _callDom().radio.checked = true;
+    return _total;
+  };
+
+  const _openResult = () => _callDom().modalSucess.className = "modal-sucess --is-sucess";
+  const _closeModalSucess = () => _callDom().modalSucess.classList.remove("--is-sucess");
+  const _returnSucess = () => setTimeout(function () {
+    _closeModalSucess()
   }, 2000);
 
-_equals = () => {
-  const totalSpan = document.querySelector("span");
-  totalSpan.innerHTML = "Total: " + _operation();
-  _openResult();
-  _returnSucess();
-  document.querySelector('input[type="radio"]').checked = false;
-  _resetStorage();
-};
+  const equals = () => {
+    _callDom().totalSpan.innerHTML = "Total: " + _operation();
+    _openResult();
+    _returnSucess();
+    _callDom().radio.checked = false;
+    _resetStorage();
+  };
+
+  return {
+    storage,
+    equals,
+    clearInput,
+  };
+})();
